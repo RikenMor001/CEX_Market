16856ece0c2ca46d18a26f28d149e22b064861cc
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-12-31 16:15:02 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use actix_web::{App, post, HttpServer, Responder};
+use actix_web::{post, App, HttpServer, Responder, HttpResponse};
+use serde::{Serialize, Deserialize};
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(move || {
+    HttpServer::new(|| {
         App::new()
-        .service(create_order)
-        .service(delete_order)
-        .service(market_depth)
+            .service(create_order)
+            .service(delete_order)
+            .service(market_depth)
     })
     .bind("127.0.0.1:8080")?
     .run()
@@ -14,19 +15,31 @@ async fn main() -> Result<(), std::io::Error> {
 }
 
 #[derive(Serialize, Deserialize)]
-
-struct Rect{
+struct Rect {
     width: u32,
-    height: u32
+    height: u32,
+}
+
+enum Side{
+    Buy,
+    Sell
+}
+
+struct trade_orders {
+    price: u32,
+    quantity: u32,
+    user_id: u32,
+    side: Side
 }
 
 #[post("/order")]
-async fn create_order(r: Request) -> impl Responder {
-    let r: Rect = Rect{
+async fn create_order(body: Json<trade_orders>) -> impl Responder { // body: Json<trade_orders> is a way to parse the request body
+    let r = Rect {
         width: 30,
-        height: 20
-    }
-    return HttpResponse::Ok().json(r);
+        height: 20,
+    };
+
+    HttpResponse::Ok().json(r)
 }
 
 #[post("/delete")]
