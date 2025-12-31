8f29b24375c275685c0fa789a84326043bd51edc
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-12-30 19:47:05 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+// Creating market depth
+// Cannot make the main function async because of the actix-web macro.
+
+#[actix_web::main]
+use actix_web::{App, HttpServer}; // This is importing the App and 
+// HttpServer types from the actix-web crate.
+async fn main() {
+
 }
