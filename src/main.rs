0043bf6b2fe644e7cb3e4b99a6ad61b2378a1b49
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-12-31 15:46:23 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
-// Creating market depth
-// Cannot make the main function async because of the actix-web macro.
+use actix_web::{App, post, HttpServer, Responder};
 
 #[actix_web::main]
-use actix_web::{App, HttpServer}; // This is importing the App and 
-// HttpServer types from the actix-web crate.
-async fn main() {
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(move || {
+        App::new()
+        .service(create_order)
+        .service(delete_order)
+        .service(market_depth)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
 
+#[derive(Serialize, Deserialize)]
+
+struct Rect{
+    width: u32,
+    height: u32
+}
+
+#[post("/order")]
+async fn create_order(r: Request) -> impl Responder {
+    let r: Rect = Rect{
+        width: 30,
+        height: 20
+    }
+    return HttpResponse::Ok().json(r);
+}
+
+#[post("/delete")]
+async fn delete_order() -> impl Responder {
+    "Order Created"
+}
+
+#[post("/depth")]
+async fn market_depth() -> impl Responder {
+    "Order Created"
 }
