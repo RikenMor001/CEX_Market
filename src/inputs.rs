9275adbe42dc9e1ab9de29829444214137abda78
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-02 18:45:53 -0500

    Update inputs.rs

--- src/inputs.rs
+++ src/inputs.rs
@@ -1,29 +1,29 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub enum Side{
+pub enum Side {
     Buy,
-    Sell
+    Sell,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct trade_orders_input { // Create a struct to represent the request body
+pub struct trade_orders_input {
     pub price: u32,
     pub quantity: u32,
     pub user_id: u32,
-    pub side: Side
+    pub side: Side,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct order_deleted_input{
-    pub order_id: u32
+pub struct order_deleted_input {
+    pub order_id: u32,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct market_depth_input{
+pub struct market_depth_input {
     pub symbol: String,
     pub quantity: u32,
     pub bids: Vec<u32>,
     pub asks: Vec<u32>,
-    pub price_spread: u32
+    pub price_spread: u32,
 }
